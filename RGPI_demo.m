93a9bbef92cc278ccdb5d4c8ff72315dbb4e2f3f
ahuyzx <34214177+ahuyzx@users.noreply.github.com>
2020-09-02 09:54:01 +0800

    Add files via upload

--- RGPI_demo.m
+++ RGPI_demo.m
@@ -1,20 +1,21 @@
-%% The source codes of RGPI and the test datasets are provided by Xiaoshuang Ma, Anhui University, 2020.
+%% The source codes of RGPI are provided by Xiaoshuang Ma, Anhui University, ChinaÅC 2020.
 %% See the reference "A Referenceless Edge Preservation Assessment Index for SAR Filters under Bayesian Framework Based on the Ratio Gradient", IEEE TGRS.
 clc
 clear all
+close all
+L=input('nominal number of looks of the speckled data: ');
+w=9;  % pixel number of the patch
 
-L=input('number of looks of the speckled data: ');
-N=9;  % Pixel number of the patch
+ addpath('data/Building','data/TerraSAR','data/ALOS','data/AirSAR');
+ %% load the speckled and filtered SAR data, all in intensity format
+ load ALOS_noisy
+ load ALOS_BM3D
+ x_noisy=ALOS_noisy; x_filtered =ALOS_BM3D;
 
-addpath('data');
- load x_noisy_real2
- load x_BM3D_real2
- x_noisy=x_noisy_real2;
- x_filtered =x_BM3D_real2;
-%  figure,imshow(uint8(x_filtered.^0.5)); % show the filtered Terrasar-x data
- figure,imshow(x_filtered.^0.5);    % show the filtered ALOS data
-        
+ figure,imshow(x_filtered.^0.5);  
 ratio=x_noisy./x_filtered; 
-figure,imshow(ratio); % show the ratio image 
+figure,imshow(ratio); 
 
-RGPI_index=RGPI(x_noisy,x_filtered,L,N)   
+tic
+RGPI_value=RGPI(x_noisy,x_filtered,L,w)   
+toc
